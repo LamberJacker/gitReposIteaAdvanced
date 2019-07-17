30f0e59dd37ede8653abea9bdc29f7f40c522ac6
HomePC <vladyslav.martynovskyi@gmail.com>
2019-07-17 17:48:03 +0300

    2nd commit

---
+++ classwork/l1/task3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace task3
+{
+    interface IGlucable
+    {
+        bool isGluck();
+    }
+    abstract class TabletPC
+    {
+        string cpu;
+        int ram;
+        public TabletPC(string cpu, int ram)
+        {
+            this.cpu = cpu;
+            this.ram = ram;
+        }
+        public virtual string GetInfo()
+        {
+            return ("CPU = " + cpu + ", RAM = " + ram);
+        }
+        public abstract void GetMark();
+    }
+    class Ipad : TabletPC , IGlucable
+    {
+        int diag;
+        public Ipad(int diag, string cpu, int ram) : base (cpu, ram)
+        {
+            this.diag = diag;
+        }
+        public override void GetMark()
+        {
+            Console.WriteLine("mark is nice");
+        }
+        public override string GetInfo()
+        {
+            return ( base.GetInfo() + " + diag = " + diag);
+        }
+        public bool isGluck()
+        {
+            return true;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.ReadKey();
+        }
+    }
+}

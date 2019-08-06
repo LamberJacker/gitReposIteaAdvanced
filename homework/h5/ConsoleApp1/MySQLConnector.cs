8041da9fb9a41d5f5920e5ae05cb568827f03f5c
LamberJack <vladmartunovskiy@gmail.com>
2019-08-06 13:25:51 +0300

    done

---
+++ homework/h5/ConsoleApp1/MySQLConnector.cs
@@ -0,0 +1,18 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class MySQLConnector
+    {
+        public static MySqlConnection getConnection(string host, int port ,string db, string username, string password)
+        {
+            String config = "Server=" + host + ";Database="  + db + ";port=" 
+                + port + ";USer id=" + username + ";Password=" + password;
+            MySqlConnection connect = new MySqlConnection(config);
+            return connect;
+        }
+    }
+}

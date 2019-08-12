546da6bdf70f0f117014696dfca94e0ecb0396ce
LamberJack <vladmartunovskiy@gmail.com>
2019-08-12 13:02:07 +0300

    create all needed method

--- homework/webregistration/lasthomework/usersDatabase/UserControler.cs
+++ homework/webregistration/lasthomework/usersDatabase/UserControler.cs
@@ -3,176 +3,110 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
+using System.Data.Common;
 
 namespace lasthomework
 {
     class UserControler
     {
-        public User GetUser(int idUser) 
+        public Boolean Login(string login, string password) 
         {
             MySqlConnection connect = DBWorker.getMySqlConnection();
             connect.Open();
-            User user = new User();
-            string query = "SELECT * FROM users WHERE id=" + idUser;
-            MySqlCommand command = new MySqlCommand();
-            command.Connection = connect;
-            command.CommandText = query;
-
+            string squery = "SELECT * FROM users";
+            MySqlCommand command = new MySqlCommand(squery, connect);
             using (DbDataReader reader = command.ExecuteReader())
             {
                 if (reader.HasRows)
                 {
                     while (reader.Read())
                     {
-                        int idO = reader.GetOrdinal("id");
-                        int nameO = reader.GetOrdinal("name");
                         int loginO = reader.GetOrdinal("login");
                         int passwordO = reader.GetOrdinal("password");
-                        int addressO = reader.GetOrdinal("address");
-                        int ageO = reader.GetOrdinal("age");
-
-                        long id = Convert.ToInt64(reader.GetValue(idO));
-                        string name = reader.GetString(nameO);
-                        string login = reader.GetString(loginO);
-                        string password = reader.GetString(passwordO);
-                        string address = reader.GetString(addressO);
-                        long age = Convert.ToInt64(reader.GetValue(ageO));
-
-                        user.id = id;
-                        user.name = name;
-                        user.login = login;
-                        user.password = password;
-                        user.address = address;
-                        user.age = age;
+                        string templogin = reader.GetString(loginO);
+                        string temppassword = reader.GetString(passwordO);
+                        if (login.Equals(templogin) & password.Equals(temppassword)) return true;
                     }
                 }
+                return false;
             }
-            connect.Close();
-            return user;
         }
-        public List<User> GetAllUser() 
+        public Boolean AddUser(string login, string password, string name, string repassword, string address, string age, int sex) 
         {
-            MySqlConnection connect = DBWorker.getMySqlConnection();
-            connect.Open();
+            bool intage = Int32.TryParse(age, out int resultage);
+            if (!intage) return false;
 
-            string query = "SELECT * FROM users";
-
-            MySqlCommand command = new MySqlCommand(query, connect);
-
-            List<User> userCol = new List<User>(); // collection for my users
-            using (DbDataReader reader = command.ExecuteReader())
+            if (name != null & login != null & password.Equals(repassword) & password != null & address != null & resultage > 0 & sex == 0 || sex == 1)
             {
-                if (reader.HasRows)
-                {
-                    while (reader.Read())
-                    {
-                        int idO = reader.GetOrdinal("id");
-                        int nameO = reader.GetOrdinal("name");
-                        int loginO = reader.GetOrdinal("login");
-                        int passwordO = reader.GetOrdinal("password");
-                        int addressO = reader.GetOrdinal("address");
-                        int ageO = reader.GetOrdinal("age");
-
-                        long id = Convert.ToInt64(reader.GetValue(idO));
-                        string name = reader.GetString(nameO);
-                        string login = reader.GetString(loginO);
-                        string password = reader.GetString(passwordO);
-                        string address = reader.GetString(addressO);
-                        long age = Convert.ToInt64(reader.GetValue(ageO));
-
-                        User tempUser = new User(); // temp user
-                        tempUser.id = id;
-                        tempUser.name = name;
-                        tempUser.login = login;
-                        tempUser.password = password;
-                        tempUser.address = address;
-                        tempUser.age = age;
-
-                        userCol.Add(tempUser);
-                    }
-                }
+                MySqlConnection connect = DBWorker.getMySqlConnection();
+                connect.Open();
+                password += "14qa15sd16df";
+                String query = "INSERT INTO users (login, password, name, address, age, sex) VALUES (@login, @password, @name," +
+                    "@address, @age, @sex)";
+                MySqlCommand command = new MySqlCommand(query, connect);
+                command.Parameters.Add(new MySqlParameter("@name", name));
+                command.Parameters.Add(new MySqlParameter("@login", login));
+                command.Parameters.Add(new MySqlParameter("@password", password));
+                command.Parameters.Add(new MySqlParameter("@address", address));
+                command.Parameters.Add(new MySqlParameter("@age", resultage));
+                command.Parameters.Add(new MySqlParameter("@sex", sex));
+                command.ExecuteNonQuery();
+                connect.Close();
+                return true;
             }
-            connect.Close();
-            return userCol;
+            else return false;
         }
-        public void DeleteUser(int idUser) 
+        public Boolean EditUser(string login, string password, string name, string address, string age, int sex) 
         {
-            MySqlConnection connect = DBWorker.getMySqlConnection();
-            connect.Open();
-            string query = "DELETE FROM users WHERE id = " + idUser + ";";
-            MySqlCommand command = new MySqlCommand(query, connect);
-            command.ExecuteNonQuery();
-            connect.Close();
-        }
-        public void AddUser(string name, string login, string password, string address, int age)
-        {
-            MySqlConnection connect = DBWorker.getMySqlConnection();
-            connect.Open();
-
-
-            string query = "INSERT INTO users (name, login, password, address, age) VALUES (@name, @login, @password," +
-                "@address, @age)";
-
-            MySqlCommand command = new MySqlCommand(query, connect);
-            command.Parameters.Add(new MySqlParameter("@name", name));
-            command.Parameters.Add(new MySqlParameter("@login", login));
-            command.Parameters.Add(new MySqlParameter("@password", password));
-            command.Parameters.Add(new MySqlParameter("@address", address));
-            command.Parameters.Add(new MySqlParameter("@age", age));
-            command.ExecuteNonQuery();
-            connect.Close();
-        }
-        public void AddNewOrderToUser(int user_id, string nameOrder, int modelOrder)
-        {
-            MySqlConnection connect = DBWorker.getMySqlConnection();
-            connect.Open();
-
-            string query = "INSERT INTO orders (user_id, name, model) VALUES (@user_id, @name, @model)";
-
-            MySqlCommand command = new MySqlCommand(query, connect);
-            command.Parameters.Add(new MySqlParameter("@user_id", user_id));
-            command.Parameters.Add(new MySqlParameter("@name", nameOrder));
-            command.Parameters.Add(new MySqlParameter("@model", modelOrder));
-            command.ExecuteNonQuery();
-            connect.Close();
-        }
-        public List<Order> GetAllOrders()
-        {
-            MySqlConnection connect = DBWorker.getMySqlConnection();
-            connect.Open();
-
-            string query = "SELECT * FROM orders";
-
-            MySqlCommand command = new MySqlCommand(query, connect);
-
-            List<Order> orderCol = new List<Order>(); // collection for my users
-            using (DbDataReader reader = command.ExecuteReader())
+            Boolean intage = Int32.TryParse(age, out int resultage);
+            if (!intage) return false;
+            if (name != null || login != null || password != null || address != null || resultage > 0 || sex == 0 || sex == 1)
             {
-                if (reader.HasRows)
+                MySqlConnection connect = DBWorker.getMySqlConnection();
+                connect.Open();
+                if (name != null)
                 {
-                    while (reader.Read())
-                    {
-                        int idO = reader.GetOrdinal("id");
-                        int user_idO = reader.GetOrdinal("user_id");
-                        int nameO = reader.GetOrdinal("name");
-                        int modelO = reader.GetOrdinal("model");
-
-                        long id = Convert.ToInt64(reader.GetValue(idO));
-                        long user_id = Convert.ToInt64(reader.GetValue(user_idO));
-                        string name = reader.GetString(nameO);
-                        int model = Convert.ToInt32(reader.GetValue(modelO));
-
-                        Order tempOrder = new Order(); // temp user
-                        tempOrder.id = id;
-                        tempOrder.user_id = user_id;
-                        tempOrder.name = name;
-                        tempOrder.model = model;
-
-                        orderCol.Add(tempOrder);
-                    }
+                    string query = "UPDATE users SET name = @name WHERE login = @login";
+                    MySqlCommand command = new MySqlCommand(query, connect);
+                    command.Parameters.Add(new MySqlParameter("@name", name));
+                    command.Parameters.Add(new MySqlParameter("@login", login));
+                    command.ExecuteNonQuery();
                 }
+                if (password != null)
+                {
+                    string query = "UPDATE users SET password = @password WHERE login = @login";
+                    MySqlCommand command = new MySqlCommand(query, connect);
+                    command.Parameters.Add(new MySqlParameter("@password", password));
+                    command.Parameters.Add(new MySqlParameter("@login", login));
+                    command.ExecuteNonQuery();
+                }
+                if (address != null)
+                {
+                    string query = "UPDATE users SET address = @address WHERE login = @login";
+                    MySqlCommand command = new MySqlCommand(query, connect);
+                    command.Parameters.Add(new MySqlParameter("@address", address));
+                    command.Parameters.Add(new MySqlParameter("@login", login));
+                    command.ExecuteNonQuery();
+                }
+                if (resultage > 0)
+                {
+                    string query = "UPDATE users SET age = @age WHERE login = @login";
+                    MySqlCommand command = new MySqlCommand(query, connect);
+                    command.Parameters.Add(new MySqlParameter("@age", age));
+                    command.Parameters.Add(new MySqlParameter("@login", login));
+                    command.ExecuteNonQuery();
+                }
+                if (sex == 0 || sex == 1)
+                {
+                    string query = "UPDATE users SET sex = @sex WHERE login = @login";
+                    MySqlCommand command = new MySqlCommand(query, connect);
+                    command.Parameters.Add(new MySqlParameter("@sex", sex));
+                    command.ExecuteNonQuery();
+                }
+                connect.Close();
+                return true;
             }
-            connect.Close();
-            return orderCol;
+            else return false;
         }
     }
+}
